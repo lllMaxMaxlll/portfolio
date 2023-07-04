134036365d19b24ac3407e99397c504608d932b9
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-04 08:05:20 -0300

    feat: landing & bg landing

---
+++ components/landing.tsx
@@ -0,0 +1,14 @@
+function Landing() {
+	return (
+		<section className="flex justify-center content-center absolute h-screen w-screen">
+			<div className="flex flex-col justify-center cursor-default">
+				<p className="mb-2 ps-2 text-start text-neutral-500 text-xl">I AM</p>
+				<h1 className="text-8xl md:text-9xl font-bold text-center">MAX</h1>
+				<p className="text-end mt-2 text-neutral-500 text-xl">A FULL STACK</p>
+				<p className="text-end -mt-1 text-neutral-500 text-xl">WEB DEVELOPER</p>
+			</div>
+		</section>
+	);
+}
+
+export default Landing;
---
+++ components/landingBg.tsx
@@ -0,0 +1,14 @@
+import { iconsLanding } from "@/utils";
+import { SvgIcon } from "@/components";
+
+function LandingBg() {
+	return (
+		<div className="absolute inset-0 -z-10 animate-fade-in overflow-hidden">
+			{iconsLanding.map((icon, index) => (
+				<SvgIcon key={index} title={icon.title} color={icon.color} d={icon.d} size={icon.size} />
+			))}
+		</div>
+	);
+}
+
+export default LandingBg;
