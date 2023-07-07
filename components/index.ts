a04b745334bbf6e010457782ae535c6820887df4
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-07 06:09:31 -0300

    feat: add barrels

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as Footer } from "./footer";
+export { default as Header } from "./header";
+export { default as Landing } from "./landing";
+export { default as LandingBg } from "./landingBg";
+export { default as SvgIcon } from "./svgIcon";
