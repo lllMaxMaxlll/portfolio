1bb46845656130cb3b4f8ca0150cd1e8e7f1502e
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-07 06:55:34 -0300

    feat: add typewriter

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,11 +1,22 @@
+"use client";
+
+import Typewriter from "typewriter-effect";
+
 function Landing() {
 	return (
 		<section className="flex justify-center content-center absolute h-screen w-screen">
-			<div className="flex flex-col justify-center cursor-default">
-				<p className="mb-2 ps-2 text-start text-neutral-500 text-xl">I AM</p>
-				<h1 className="text-8xl md:text-9xl font-bold text-center">MAX</h1>
-				<p className="text-end mt-2 text-neutral-500 text-xl">A FULL STACK</p>
-				<p className="text-end -mt-1 text-neutral-500 text-xl">WEB DEVELOPER</p>
+			<div className="flex flex-col justify-center">
+				<p className="mb-2 ps-2 text-start text-neutral-500 text-base md:text-xl">I AM</p>
+				<h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-bold text-center">MAX HERR</h1>
+				<div className="text-end mt-2 text-neutral-500 text-base md:text-xl">
+					<Typewriter
+						options={{
+							strings: ["A FRONT END DEVELOPER", "A BACK END DEVELOPER", "A PHOTOGRAPHER"],
+							autoStart: true,
+							loop: true,
+						}}
+					/>
+				</div>
 			</div>
 		</section>
 	);
