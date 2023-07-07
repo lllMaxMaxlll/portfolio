24ac660436aeefa4f629f0e0a88f84cefeb02d7c
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-07 06:10:13 -0300

    fix: add hidden small screen

--- components/landingBg.tsx
+++ components/landingBg.tsx
@@ -1,9 +1,9 @@
-import { iconsLanding } from "@/utils";
-import { SvgIcon } from "@/components";
+import { iconsLanding } from "../utils";
+import { SvgIcon } from "../components";
 
 function LandingBg() {
 	return (
-		<div className="absolute inset-0 -z-10 animate-fade-in overflow-hidden">
+		<div className="hidden lg:block absolute inset-0 -z-10 animate-fade-in overflow-hidden">
 			{iconsLanding.map((icon, index) => (
 				<SvgIcon key={index} title={icon.title} color={icon.color} d={icon.d} size={icon.size} />
 			))}
