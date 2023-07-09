c3a79cf3354bfa162752953df4ff2c394daeddd0
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-09 10:50:29 -0300

    feat: add under contruction title

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+	return (
+		<section className="flex justify-center content-center absolute h-screen w-screen">
+			<div className="flex flex-col justify-center">
+				<h2 className="text-6xl font-bold text-center">UNDER CONSTRUCTION</h2>
+			</div>
+		</section>
+	);
+}
