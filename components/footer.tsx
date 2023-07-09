c468076ccdc4cbc7f02de636abac031191655e3d
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-09 10:50:46 -0300

    fix: center icons footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ function Footer() {
 	return (
 		<footer className="fixed w-screen bottom-8">
 			<div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-16 py-2">
-				<div className="flex items-center md:gap-12 justify-center md:justify-end">
-					<ul className="flex md:flex-col items-center gap-6 text-sm ">
+				<div className="flex items-center md:gap-12 justify-center ">
+					<ul className="flex items-center gap-6 text-sm ">
 						<a className="fill-neutral-500 transition hover:fill-neutral-200/75" href="https://github.com/lllMaxMaxlll" target="_blank">
 							<Github />
 						</a>
