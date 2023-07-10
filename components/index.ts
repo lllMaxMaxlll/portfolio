e58d5eb24291ac128734d14fcdff9427fd1a9384
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-10 18:57:29 -0300

    fix: update barrel

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as Landing } from "./landing";
+export { default as NavMobile } from "./navMobile";
+export { default as CardCarousel } from "./cardCarousel";
+export { default as Carousel3d } from "./carousel3d";
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,8 @@
+export { default as ChevronLeft } from "./carouselIcons/left";
+export { default as ChevronRight } from "./carouselIcons/right";
 export { default as Github } from "./footerIcons/githubIcon";
 export { default as Instagram } from "./footerIcons/instagramIcon";
 export { default as Linkedin } from "./footerIcons/linkedinIcon";
+export { default as Mail } from "./footerIcons/mailIcon";
 export { default as useMousePosition } from "./mouse";
+export { projects } from "./projects";
