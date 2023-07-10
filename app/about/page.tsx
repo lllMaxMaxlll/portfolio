207a088d89b9628fa343548503fb816d97f591a0
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-10 20:09:44 -0300

    fix: responsive temp about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
 	return (
 		<section className="flex justify-center content-center absolute h-screen w-screen">
 			<div className="flex flex-col justify-center">
-				<h2 className="text-6xl font-bold text-center">UNDER CONSTRUCTION</h2>
+				<h2 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center">UNDER CONSTRUCTION</h2>
 			</div>
 		</section>
 	);
