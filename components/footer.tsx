c69f5fcf8b0cd7bc3855c3948d12663299931cd9
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-10 21:18:30 -0300

    fix: footer position

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,10 +2,10 @@ import { Instagram, Linkedin, Github, Mail } from "../utils";
 
 function Footer() {
 	return (
-		<footer className="fixed w-screen bottom-8 z-30">
-			<div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-16 py-2">
-				<div className="flex justify-center md:justify-end ">
-					<ul className="flex items-center md:flex-col gap-4 text-sm ">
+		<footer className="fixed bottom-5 w-screen z-50">
+			<div className="mx-auto max-w-screen-xl px-5 sm:px-6 lg:px-16 py-2">
+				<div className="flex justify-center items-center">
+					<ul className="flex items-center justify-center gap-3">
 						<a className="stroke-neutral-500 transition hover:stroke-red-600/75" href="mailto:max.herr.88@gmail.com" target="_blank">
 							<Mail />
 						</a>
