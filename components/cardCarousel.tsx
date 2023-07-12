aad7213fc33adeb114fc9a2b9736920d4527205a
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-12 00:04:04 -0300

    fix: icons position

--- components/cardCarousel.tsx
+++ components/cardCarousel.tsx
@@ -8,7 +8,7 @@ function CardCarousel({ image, title, description, github, demo }: ProjectData) 
 			<Image src={image} alt={title} width="700" height="300" />
 			<h2 className="text-xl sm:text-3xl font-bold pt-5">{title}</h2>
 			<p className="pt-2 font-thin">{description}</p>
-			<div className="absolute -bottom-11 flex p-3 opacity-0 transition group-hover:translate-y-4 group-hover:opacity-100 ">
+			<div className="absolute -bottom-16 sm:-bottom-18 flex p-3 md:opacity-0 transition sm:group-hover:-translate-y-1 group-hover:opacity-100 ">
 				{demo && (
 					<a href={demo} className="px-3 hover:opacity-75 transition" target="_blank" role="button">
 						<LinkIcon />
