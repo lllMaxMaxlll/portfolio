8b21eafbde9dc113646e6ff010ad305a439cbebf
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-13 08:54:12 -0300

    fix: width and height, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,14 @@ const rubik = Rubik({ subsets: ["latin"] });
 export const metadata = {
 	title: "Max Herr Portfolio",
 	description: "Max Herr Full Stack Web Developer",
-	keywords: "Max Herr, Portfolio, Web Developer",
+	keywords: "Max Herr, Portfolio, Web Developer, Full Stack",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body
-				className={`${rubik.className} text-neutral-900 bg-neutral-200 dark:text-neutral-100 dark:bg-neutral-900 overflow-hidden`}
+				className={`${rubik.className} text-neutral-900 bg-neutral-200 dark:text-neutral-100 dark:bg-neutral-900 overflow-hidden absolute top-0 w-screen h-screen`}
 				suppressHydrationWarning>
 				<Providers>{children}</Providers>
 			</body>
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,9 @@ import { Instagram, Linkedin, Github, Mail } from "../utils";
 
 function Footer() {
 	return (
-		<footer className="fixed bottom-5 w-screen z-50">
-			<div className="mx-auto max-w-screen-xl px-5 sm:px-6 lg:px-16 py-2">
-				<div className="flex justify-center items-center">
+		<footer className="fixed bottom-0 left-0 right-0 w-screen z-50">
+			<div className="mx-auto px-5 sm:px-6 lg:px-16 py-2">
+				<div className="flex justify-center items-center my-3">
 					<ul className="flex items-center justify-center gap-3">
 						<a
 							className="stroke-neutral-900 dark:stroke-neutral-200 transition hover:stroke-red-600/75 dark:hover:stroke-red-600/75"
