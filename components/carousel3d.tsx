d21264909d3d0ba269f3f7bb3cf25add1ba649d5
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-14 02:25:32 -0300

    fix: delete some trash lines

--- components/carousel3d.tsx
+++ components/carousel3d.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { ChevronLeft, ChevronRight, Github, LinkIcon, projects } from "@/utils";
 import Image from "next/image";
 
