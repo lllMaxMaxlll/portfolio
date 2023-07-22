217725344465a21f9d7cb135835eb7bd24710942
lllMaxMaxlll <max.herr.88@gmail.com>
2023-07-22 00:20:30 -0300

    fix: motion frame

---
+++ components/aboutMe.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { aboutIcons } from "@/utils";
+import { useTranslations } from "next-intl";
+import { motion } from "framer-motion";
+
+function AboutMe() {
+	const t = useTranslations("About");
+
+	return (
+		<motion.div
+			initial={{ opacity: 0, translateY: 15 }}
+			animate={{ opacity: 1, translateY: 0 }}
+			className="flex flex-col md:flex-row justify-center content-center h-[80vh] gap-5 mx-auto w-3/4 md:max-w-3xl">
+			<div className="flex flex-col justify-center content-center text-center">
+				<h2 className="text-2xl sm:text-4xl font-bold text-center py-5">{t("title")}</h2>
+				<p>{t("firstP")}</p>
+				<br />
+				<p className="hidden sm:block">{t("secondP")}</p>
+				<div className="grid grid-cols-6 md:grid-cols-12 gap-6 mx-auto py-5">
+					{aboutIcons.map((icon, i) => (
+						<svg
+							key={i}
+							role="img"
+							width={24}
+							height={24}
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+							className=" fill-neutral-900 dark:fill-neutral-200">
+							<title>{icon.title}</title>
+							<path d={icon.path} />
+						</svg>
+					))}
+				</div>
+			</div>
+		</motion.div>
+	);
+}
+
+export default AboutMe;
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as Landing } from "./landing";
+export { default as AboutMe } from "./aboutMe";
 export { default as NavMobile } from "./navMobile";
 export { default as Carousel3d } from "./carousel3d";
 export { default as SwitcherTheme } from "./switcherTheme";
