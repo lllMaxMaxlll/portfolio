98937b1db6995296fc79d7499ec04f8b8506c151
lllMaxMaxlll <max.herr.88@gmail.com>
2025-01-23 21:05:11 -0300

    feat: add plugin syntax highlighter that provides beautiful code blocks for Markdown, delete older one

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,23 +3,31 @@ import html from "remark-html";
 import { rehype } from "rehype";
 import rehypeRaw from "rehype-raw";
 import rehypeSanitize from "rehype-sanitize";
-import rehypeHighlight from "rehype-highlight";
-
+import { transformerCopyButton } from "@rehype-pretty/transformers";
+import rehypePrettyCode from "rehype-pretty-code";
 
 export async function markdownToHtml(markdown: string): Promise<string> {
-  const processedContent = await remark()
-    .use(html) // Converts Markdown to HTML
-    .process(markdown);
+	const processedContent = await remark()
+		.use(html) // Converts Markdown to HTML
+		.process(markdown);
 
-  const htmlContent = processedContent.toString();
+	const htmlContent = processedContent.toString();
 
-  // Rehype processes raw HTML and sanitizes it
-  const sanitizedContent = await rehype()
-    .data("settings", { fragment: true }) // Allows partial HTML fragments
-    .use(rehypeRaw) // Processes raw HTML embedded in Markdown
-    .use(rehypeSanitize) // Sanitizes the HTML for safety
-    .use(rehypeHighlight) // Highlights code blocks
-    .process(htmlContent);
+	// Rehype processes raw HTML and sanitizes it
+	const sanitizedContent = await rehype()
+		.data("settings", { fragment: true }) // Allows partial HTML fragments
+		.use(rehypeRaw) // Processes raw HTML embedded in Markdown
+		.use(rehypeSanitize) // Sanitizes the HTML for safety
+		.use(rehypePrettyCode, {
+			// Adds syntax highlighting and copy button on hover
+			transformers: [
+				transformerCopyButton({
+					visibility: "hover",
+					feedbackDuration: 3_000,
+				}),
+			],
+		})
+		.process(htmlContent);
 
-  return sanitizedContent.toString();
+	return sanitizedContent.toString();
 }
